195636cf111b257255174f7e67637c6e2136b782
kurpachsv <kurpachsv@gmail.com>
2019-03-23 13:49:32 +0300

    Add multiplier prop

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import _ from 'lodash';
 
 export default () => (WrappedComponent) => {
     return class extends React.Component {
+        static propTypes = {
+            multiplier: PropTypes.number,
+        };
+        static defaultProps = {
+            multiplier: 80,
+        };
+
         constructor(props) {
             super(props);
             this.currentY = 0;
         }
 
-        getScrollY(clientY) {
+        getScrollY = (clientY) => {
             const sign = Math.sign(clientY - this.currentY);
-            return Math.round(sign * 80);
-        }
+            return Math.round(sign * this.props.multiplier);
+        };
 
         updateScrolling = _.throttle((e) => {
             window.scrollBy(0, this.getScrollY(e.clientY));
